43365b833960174c8c11d9786a5d695b3e862c51
Magnus Landerblom <magnus@landerblom.se>
2023-08-23 11:23:22 +0200

    Update to newer Cargo version

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,38 +1,37 @@
-use clap::{AppSettings, Clap};
+use clap::{Parser, Subcommand};
 use ini::Ini;
 
 /// This doc string acts as a help message when the user runs '--help'
 /// as do all doc strings on fields
-#[derive(Clap)]
-#[clap(version = "1.0", author = "Magnus L. <mange@84codes.com>")]
-#[clap(setting = AppSettings::ColoredHelp)]
+#[derive(Parser)]
+#[command(version = "1.0", author = "Magnus L. <mange@84codes.com>")]
 struct Opts {
     /// A level of verbosity, and can be used multiple times
-    #[clap(short, long, parse(from_occurrences))]
+    #[arg(short, long, action = clap::ArgAction::Count)]
     verbose: i32,
 
-    #[clap(subcommand)]
+    #[command(subcommand)]
     subcmd: SubCommand,
 
     /// Config file for sparoid
-    #[clap(long, short, default_value = "~/.sparoid.ini")]
+    #[arg(long, short, default_value = "~/.sparoid.ini")]
     config_file: String,
 }
 
-#[derive(Clap)]
+#[derive(Subcommand)]
 enum SubCommand {
-    #[clap(version = "1.0", author = "Someone E. <someone_else@other.com>")]
+    #[arg()]
     Send(Connect),
 }
 
 /// Configuration
-#[derive(Clap, Debug)]
+#[derive(Parser, Debug)]
 struct Connect {
     /// Host to send package to
-    #[clap(long)]
+    #[arg(long)]
     host: String,
     /// Port on which to send package to
-    #[clap(long, default_value = "8484")]
+    #[arg(long, default_value = "8484")]
     port: u16,
 }
 
